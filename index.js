07ef6555dc5aefe7f50c82a3cb594ecda32e94f2
asharmarx <aman.sharmarx@gmail.com>
2022-10-30 00:25:24 -0400

    Rennamed to index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 8080;
+const whatIRead = require("./whatiread");
+const whatIHaveDone = require("./whatihavedone");
+
+app.use(express.json());
+app.use(cors());
+app.get("/", (req, res) => {
+  res.end("pong");
+});
+app.get("/what-i-read", (req, res) => {
+  res.send(whatIRead);
+});
+app.get("/what-i-have-done", (req, res) => {
+  res.send(whatIHaveDone);
+});
+
+app.listen(port, () => console.log(`brain listening on port ${port}!`));
