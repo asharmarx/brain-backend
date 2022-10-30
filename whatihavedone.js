c55670dac878a4b7cf8230cd677c172f7d090db0
asharmarx <aman.sharmarx@gmail.com>
2022-10-30 00:25:48 -0400

    added data

---
+++ whatihavedone.js
@@ -0,0 +1,28 @@
+const whatIHaveDone = [
+  {
+    id: 1,
+    title: "bizarro collective",
+    imageLink: "",
+    description: `bizarro is a fashion collective I started in 2020 to bring artists from all around the world together and give them a platform to represent their work. The artists were paid fairly for their art and 10% of profits from each sale were donated by bizarro to a charity of the artist's choice`,
+  },
+  {
+    id: 2,
+    title: "baking",
+    imageLink: "",
+    description: `I started baking bread late 2021. I wanted to find a way to make the best bread in the simplest way. I use honey, yeast, flour, salt, and water and the prep takes 10 minutes.`,
+  },
+  {
+    id: 3,
+    title: "bike lights",
+    imageLink: "",
+    description: `I wanted to do project using the Pi-Pico and decided to make bike lights. These microcontrollers cost only $4 and are super easy to use`,
+  },
+  {
+    id: 4,
+    title: "baking",
+    imageLink: "",
+    description: `I started baking bread late 2021. I wanted to find a way to make the best bread in the simplest way. I use honey, yeast, flour, salt, and water and the prep takes 10 minutes.`,
+  },
+];
+
+module.exports = { whatIHaveDone };
---
+++ whatiread.js
@@ -0,0 +1,154 @@
+const whatIRead = [
+  {
+    id: 1,
+    title: "The Cask of Amontillado - Edgar Allan Poe",
+    summary: "dude buries someone in his cellar for revenge",
+    resources: [
+      {
+        title: "PDF",
+        link: "https://americanenglish.state.gov/files/ae/resource_files/the_cask_of_amontillado.pdf",
+      },
+    ],
+    seeAlso: [],
+  },
+  {
+    id: 2,
+    title: "Zero Point Energy",
+    summary:
+      "the vibrational energy that exists in quantum mechanical systems even at 0K",
+    resources: [
+      {
+        title: "Wikipedia",
+        link: "https://en.wikipedia.org/wiki/Zero-point_energy",
+      },
+    ],
+    seeAlso: [],
+  },
+  {
+    id: 3,
+    title: "Y-Most Common Recent Ancestor",
+    summary: "the most common recent ancestor of all living human males",
+    resources: [
+      {
+        title: "NCBI",
+        link: "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC16550/",
+      },
+      {
+        title: "Wikipedia",
+        link: "https://en.wikipedia.org/wiki/Y-chromosomal_Adam",
+      },
+    ],
+    seeAlso: [],
+  },
+  {
+    id: 4,
+    title: "The Nature of Fascism by Roger Griffin",
+    summary:
+      "book that talks about how people think and the complexity of that affects how difficult it is to come to a singular definition for an ideology eg fascism.",
+    resources: [],
+    seeAlso: [
+      {
+        title: "palingenetic ultranationalism",
+        link: "https://en.wikipedia.org/wiki/Palingenetic_ultranationalism",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Double Irish",
+    summary:
+      "a technique by which companies avoid paying taxes. Essentially, US Company develops tech for $x, sells it to a company in Bermuda for $x, which appreciates it to $100x (no gains tax in Bermuda). The Bermuda company licenses it to Irland_1 for $100x which licenses it Ireland_2 for $100x which sells it customer in Germany for $100x. They money is then pushed back to Bermuda as royalty payments which loans it back to US Company",
+    resources: [],
+    seeAlso: [],
+  },
+  {
+    id: 6,
+    title: "Single Malt",
+    summary:
+      "double irish got some heat, so companies use this to avoid paying taxes. Malt because Malta is used",
+    resources: [],
+    seeAlso: [],
+  },
+  {
+    id: 7,
+    title: "Treasure Islands",
+    summary: "paper about tax havens, and how they operate",
+    resources: [
+      {
+        title: "Sci-Hub",
+        link: "https://sci-hub.se/10.1257/jep.24.4.103",
+      },
+    ],
+    seeAlso: [],
+  },
+  {
+    id: 8,
+    title: "War Is Not Part of Human Nature",
+    summary: "article about how war originated within humans",
+    resources: [
+      {
+        title: "Scientific American",
+        link: "https://www.scientificamerican.com/article/war-is-not-part-of-human-nature/",
+      },
+    ],
+    seeAlso: [],
+  },
+  {
+    id: 9,
+    title: "Co-Moving Distance",
+    summary:
+      "co-moving distance between two object does not change as they move further apart",
+    resources: [{ title: "Co-Moving Distance", link: "" }],
+    seeAlso: [],
+  },
+  {
+    id: 10,
+    title: "Next Turn Repair Initiators",
+    summary:
+      "Words or phrases in language that, when engaged in a conversation, one party uses to inform the other that they have not understood them.",
+    resources: [
+      {
+        title: "Sci-Hub - Journal Of Pragmatics",
+        link: "https://sci-hub.se/10.1016/s0378-2166(97)89759-7",
+      },
+    ],
+  },
+  {
+    id: 11,
+    title: "Dual Inheritance Theory",
+    summary:
+      "The theory relating to the evolution of culture and genetics together. It talks about how humans are imitation machines and it is because of this imitation we were able to pass down useful cultural knowledge which, in turn, impacted genetic evolution.",
+    resources: [
+      {
+        title: "Sci-Hub - Oxford Online Handbook",
+        link: "https://sci-hub.se/10.1093/oxfordhb/9780198568308.013.0038",
+      },
+    ],
+  },
+  {
+    id: 12,
+    title: "The Government Securities Market: In the Wake of ESM",
+    summary:
+      "Talks about repurchase agreements (repos), and what regulations should be introduced to make them more corruption proof",
+    resources: [
+      {
+        title: "Santa Clara Law Review",
+        link: "https://digitalcommons.law.scu.edu/cgi/viewcontent.cgi?referer=&httpsredir=1&article=1842&context=lawreview",
+      },
+    ],
+  },
+  {
+    id: 13,
+    title: "Piaget's Theory of Cognitive Development",
+    summary:
+      "Breaks down the development of the brain from birth to adulthood, and how humans gather data about their environment to make sense out of it",
+    resources: [
+      {
+        title: "Wikipedia",
+        link: "https://en.wikipedia.org/wiki/Piaget%27s_theory_of_cognitive_development",
+      },
+    ],
+  },
+];
+
+module.exports = { whatIRead };
