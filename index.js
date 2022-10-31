54894f79d57d46cdd74c6ca114a07590e8ff2a75
asharmarx <aman.sharmarx@gmail.com>
2022-10-31 00:13:15 -0400

    fixed url

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const whatIRead = require("./whatiread");
 const whatIHaveDone = require("./whatihavedone");
 
 app.use(express.json());
-app.use(cors({ origin: ["https://aman.monster", "http:localhost:3000"] }));
+app.use(cors({ origin: ["https://aman.monster", "http://localhost:3000"] }));
 app.get("/", (req, res) => {
   res.end("pong");
 });
