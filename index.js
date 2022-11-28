165848a682f045b5ab825fa99a5d395adc402589
asharmarx <aman.sharmarx@gmail.com>
2022-11-27 22:46:16 -0500

    added limiter and only get method

--- index.js
+++ index.js
@@ -3,20 +3,32 @@ const app = express();
 const port = process.env.PROD_PORT || 9443;
 const whatIRead = require("./whatiread");
 const whatIHaveDone = require("./whatihavedone");
+import rateLimit from "express-rate-limit";
+
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 15 minutes
+  max: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
 app.use(express.json());
+app.use(limiter);
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://aman.monster");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  next();
+
   next();
 });
 app.get("/", (req, res) => {
   res.end("this is my bum");
 });
 app.get("/what-i-read", (req, res) => {
-  res.send(whatIRead);
+  return res.send(whatIRead);
 });
 app.get("/what-i-have-done", (req, res) => {
-  res.send(whatIHaveDone);
+  return res.send(whatIHaveDone);
 });
 
 app.listen(port, () => console.log(`brain listening on port ${port}!`));
