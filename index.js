b6f68b94dc2eb63713e0302182d90fed8aede680
asharmarx <aman.sharmarx@gmail.com>
2022-11-30 18:12:41 -0500

    adjusted limiter and added req origin logic

--- index.js
+++ index.js
@@ -3,19 +3,24 @@ const app = express();
 const port = process.env.PROD_PORT || 9443;
 const whatIRead = require("./whatiread");
 const whatIHaveDone = require("./whatihavedone");
-import rateLimit from "express-rate-limit";
+const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
-  windowMs: 5 * 60 * 1000, // 15 minutes
-  max: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  windowMs: 15 * 60 * 1000, // 5 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
 
+const whereDatComeFrom =
+  process.env.NODE_ENV === "production"
+    ? "https://aman.monster"
+    : "http://localhost:3000";
+
 app.use(express.json());
 app.use(limiter);
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
+  res.setHeader("Access-Control-Allow-Origin", whereDatComeFrom);
   res.setHeader("Access-Control-Allow-Methods", "GET");
   next();
 
